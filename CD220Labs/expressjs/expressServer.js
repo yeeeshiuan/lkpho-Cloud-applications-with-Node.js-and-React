74b900fd2cd9268cc15d0224aba1dec5fdc79425
Joey Huang <yeeeshiuan.huang@gmail.com>
2024-04-08 23:51:38 -0400

    update

--- CD220Labs/expressjs/expressServer.js
+++ CD220Labs/expressjs/expressServer.js
@@ -3,6 +3,19 @@ const app = new express();
 
 let loginDetails = [];
 
+const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+app.get("/fetchMonth/:num",(req,res)=>{
+    let num = parseInt(req.params.num);
+    if(num <1 || num >12) {
+        res.send("Not a valid month number")
+    } else {
+        res.send(months[num-1])
+    }
+    
+
+})
+
 app.get("/",(req,res)=>{
     res.send("Welcome to the express server")
 })
